45d395df6951965cf57ea8f9e401c9bedb1e5824
Huch0 <heochiyeong@gmail.com>
2024-07-25 19:55:30 +0900

    Title: 금 채굴하기, Time: 195ms, Memory: 28MB, Status: Passed - CodeTree

--- 240725/금 채굴하기/gold-mining.py
+++ 240725/금 채굴하기/gold-mining.py
@@ -6,27 +6,26 @@ mat = []
 for _ in range(n):
     mat.append(list(map(int, input().split())))
 
-
+# Make masks
 combs = list(combinations_with_replacement(range(-n, n + 1), 2))
 masks = [[] for _ in range(n + 1)]
 for c in combs:
-    i = sum(map(abs, c))
-    if i < n:
+    i = sum(map(abs, c)) # Manhattan distance
+    if i <= n:
         masks[i].append(c)
-        if c[0] != c[1]:
+        if c[0] != c[1]: # if x == y, no need to append reversed one
             masks[i].append(tuple(reversed(c)))
 
 max_coin = 0
 for i in range(n):
     for j in range(n):
         coins = 0
-        for K in range(n):
+        for K in range(n + 1):
             profit = -(K * K + (K + 1) * (K + 1))
             for dx, dy in masks[K]:
                 nx, ny = i + dx, j + dy
                 if 0 <= nx < n and 0 <= ny < n:
                     coins += mat[nx][ny]
-            
             profit += m * coins
             if profit >= 0:
                 max_coin = max(max_coin, coins)
