f2976e3fe98b8d7968f5dd2de051efbc4da4150d
Huch0 <heochiyeong@gmail.com>
2024-08-03 21:22:43 +0900

    Title: 1차원 젠가, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/1차원 젠가/jenga-1d.py
+++ 240803/1차원 젠가/jenga-1d.py
@@ -11,22 +11,10 @@ for _ in range(2):
 n_blocks = n
 for s, e in removes:
     jenga[s-1:e] = [0] * (e - s + 1)
-
-    down = -1
-    for up in range(-1, -n_blocks-1, -1):
-        if jenga[down] > 0:
-            down -= 1
-            continue
-        
-        # if up != down:
-        if jenga[up] > 0:
-            jenga[down], jenga[up] = jenga[up], 0
-            down = up
-
     n_blocks -= e - s + 1
-    jenga[:] = jenga[-n_blocks:]
+    jenga = [block for block in jenga if block > 0]
+
 
 print(n_blocks)
 for block in jenga:
-    if block > 0:
-        print(block)
+    print(block)
