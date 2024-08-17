46e7f9f7494991940e26ae7b1549a912b6be3322
Huch0 <heochiyeong@gmail.com>
2024-08-17 12:19:48 +0900

    Title: n개 중에 m개 뽑기, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240817/n개 중에 m개 뽑기/n-choose-m.py
+++ 240817/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,16 +1,23 @@
 N, M = map(int, input().split())
 
-combs = []
+# combs = []
 
-def recur(comb, last):
-    if len(comb) == M:
-        combs.append(comb)
-        return
+# def recur(comb, last):
+#     if len(comb) == M:
+#         combs.append(comb)
+#         return
     
-    for i in range(last + 1, N + 1):
-        recur(comb + [i], i)
+#     for i in range(last + 1, N + 1):
+#         recur(comb + [i], i)
 
-recur([], 0)
+# recur([], 0)
+
+# for comb in combs:
+#     print(' '.join(map(str, comb)))
+
+from itertools import combinations
+
+combs = combinations(range(1, N + 1), M)
 
 for comb in combs:
     print(' '.join(map(str, comb)))
